e1e0292aab615420f972cb3f5f3661a29d8cda8e
raixxx-thesis-git <rai.machine.learning.explore@gmail.com>
2024-09-04 22:54:09 +0700

    Update: virutal_backprop_system.py

--- nodeleys/system/virtual_backprop_system.py
+++ nodeleys/system/virtual_backprop_system.py
@@ -8,16 +8,18 @@ class DynamicBackpropSystem():
   def __init__(self): pass
   
   def propagate(self: Virtual):
+    idx = 0
     for virtual_graph, conditions in (self.virtual_graphs, self.conditions):
       '''
       This is a subsystem routine. We do not pass the inherited backprop states to
       simulate back propagation routines of virtual graphs (dynamic).
       Since the graphs are virtual, we do not record the back propagation traces.
       '''
-      _, _ = virtual_graph.get_adic().propagate([], [], [], interrupts=self.vars)
+      _, _ = virtual_graph.get_adic().propagate([], [], [], interrupts=self.vars, 
+                                                is_virtually=True, idx=idx)
       
       '''
       All of the interrupt adics are updated.
       '''
-      
+      idx += 1
   pass
