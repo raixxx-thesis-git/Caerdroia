2e87b06013319de087a8406bbf026bd67a8f4e70
raixx-thesis-git <rai.machine.learning.explore@gmail.com>
2024-10-17 00:25:12 +0700

    Fixing __init__.py in system

--- nodeleys/system/__init__.py
+++ nodeleys/system/__init__.py
@@ -1,9 +1,7 @@
 from .secure_type import secure_type
-from .system import System
 from .grad_system import compute_grad
 from .duplet_backprop_system import DupletBackpropSystem
 from .triplet_backprop_system import TripletBackpropSystem
-from .virtual_backprop_system import VirtualBackpropSystem
 from .switch_backprop_system import SwitchBackpropSystem
 from . import misc
 
